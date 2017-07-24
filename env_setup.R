6b55e10a262f60786614b46ec4e731c591f97ddd
ryusko2 <ryusko2@illinois.edu>
2017-07-24 04:16:13 -0400

    Updated analysis & env_setup

--- env_setup.R
+++ env_setup.R
@@ -13,6 +13,8 @@ flight_edges = readr::read_csv("flight_edges.csv", col_types = "icccccccccclliii
 flight_edges = flight_edges[,2:ncol(flight_edges)]
 
 #set up factor variables
+flight_edges$origin_state = as.factor(flight_edges$origin_state)
+flight_edges$dest_state = as.factor(flight_edges$dest_state)
 flight_edges$origin_regn = as.factor(flight_edges$origin_regn)
 flight_edges$origin_subr = as.factor(flight_edges$origin_subr)
 flight_edges$dest_regn = as.factor(flight_edges$dest_regn)
@@ -25,11 +27,17 @@ flight_edges$month.chr = as.factor(flight_edges$month.chr)
 #flights.  In either case, from an airline perspective, we are not interested in
 #these flights.  They represent ~ 10% of the data.  
 flight_data = subset(flight_edges, flight_edges$seats > 0)
-rm(flight_edges)
+
+#pp ~ positive passenger values
+flight_data_pp = subset(flight_data, flight_data$passengers > 0)
 
 #simple sampling
 #subset all flights from January 2000 (>13K observations)
-#flight_sub_200001 = subset(flight_data, flight_data$date == 200001)
+#flight_sub_200001 = subset(flight_data_pp, flight_data_pp$date == 200001)
 
 #sample 500 of the January 2000 flights 
 #flight_sub_500 = flight_sub_200001[sample(1:nrow(flight_sub_200001), 500),]
+
+#full 3.2M observations currently stored in `flight_data`
+sub_obs = 10000
+flight_sub = flight_data_pp[sample(1:nrow(flight_data_pp), sub_obs), -c(1:3, 7, 17)]
